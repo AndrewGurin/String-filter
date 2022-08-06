923511cdfbb218b91386dadd1c7ea1e2c8f8f0f8
Andrew Gurin <GAM-Yaroslavl@mail.ru>
2022-08-06 16:28:00 +0300

    Обновление до v1.1. Ликвидировано создание второго массива

--- Program.cs
+++ Program.cs
@@ -5,45 +5,60 @@
     {
         Console.Write(array[i]);
         if (i < length - 1)
-        {
             Console.Write(", ");
+    }
+    Console.WriteLine("]");
+}
+
+void PrintFilteredArray(string?[] array, int length, int threshold)
+{
+    bool begin = false;
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i]!.Length <= threshold)
+        {
+            if (begin)
+                Console.Write(", ");
+            Console.Write(array[i]);
+            begin = true;
         }
     }
     Console.WriteLine("]");
 }
 
-int length = 10;
+int length = 100;
 string?[] inputArray = new string[length];
-string?[] result = new string[length];
-int threshold = 3;
-int i, count = 0;
+int count = 0;
 
 Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine("Добро пожаловать в приложение String filter. v1.0!");
+Console.WriteLine("Добро пожаловать в приложение String filter. v1.1!");
 Console.ResetColor();
-Console.WriteLine($"Вводите строки и нажимайте Enter. Программа покажет строки длиной не больше {threshold} символов.");
-for (i = 0; i < length; i++)
+Console.WriteLine($"Вводите строки и нажимайте Enter. Для окончания ввода оставьте поле пустым.");
+for (count = 0; count < length; count++)
 {
-    Console.Write($"Осталось свободных ячеек: {length - i}\n> ");
-    inputArray[i] = Console.ReadLine();
-    if (inputArray[i] == string.Empty)
+    Console.Write($"Осталось свободных ячеек: {length - count}\n> ");
+    inputArray[count] = Console.ReadLine();
+    if (inputArray[count] == string.Empty)
         break;
-    if (inputArray[i]!.Length <= threshold)
-    {
-        result[count] = inputArray[i];
-        count++;
-    }
 }
 
 Console.WriteLine();
 Console.WriteLine("Исходный массив строк:");
-PrintArray(inputArray, i);
+PrintArray(inputArray, count);
 Console.WriteLine();
+Console.Write("Введите пороговое значение фильтрации:\n> ");
+int threshold = Convert.ToInt32(Console.ReadLine());
+while (threshold < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Х----------ОШИБКА!----------Х");
+    Console.WriteLine("Пороговое значение не может быть отрицательным. Повторите ввод.");
+    Console.Write("> ");
+    Console.ResetColor();
+    threshold = Convert.ToInt32(Console.ReadLine());
+}
 Console.WriteLine($"Массив строк после фильтрации по длине не больше {threshold} символов:");
-PrintArray(result, count);
+PrintFilteredArray(inputArray, count, threshold);
 Console.WriteLine();
-
-/*Console.WriteLine("Исходный массив строк:\n[" + string.Join(", ", inputArray) + "]");
-Console.WriteLine($"Массив строк после фильтрации по длине не больше {threshold}:");
-Console.WriteLine("[" + string.Join(", ", result) + "]");*/
